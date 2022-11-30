923f9ad0c6e310d3274d0b596677acaf5a019544
skrip04ka <fury.2489@gmail.com>
2022-11-30 22:15:01 +0300

    First commit

---
+++ src/main/java/org/example/PacketHelper.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import lombok.SneakyThrows;
+
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+
+public class PacketHelper {
+    private String iface;
+
+    public PacketHelper(String interfaceName) {
+        this.iface = interfaceName;
+    }
+
+    /**
+     * parses raw packet data into string value of payload
+     * @param data - all data from UDP packet
+     * @return string value of payload
+     */
+    public String parse(byte[] data){
+        if (data.length < 14) return null;
+        int offset = (iface.equals("\\Device\\NPF_Loopback") ? 4 /*local*/ : 14 /*ethernet*/) + 20 /*ipv4*/ + 8 /* udp */;
+
+        byte[]dataByte = new byte[data.length-offset];
+        System.arraycopy(data,offset,dataByte,0,dataByte.length);
+        return new String(dataByte);
+    }
+
+    /**
+     * creates packet data (byte array) of given String value
+     * @param data - string value to decode into packet
+     * @param portToSend - port to send data vid UDP
+     * @return byte arr of packet to send vid UDP
+     */
+    @SneakyThrows
+    public byte[] collectPacket(String data, int portToSend) {
+        byte[] platformDescBytes = data.getBytes();
+        int dataLength = platformDescBytes.length;
+        int totalLength = dataLength + 28;
+
+        byte [] packet = new byte[totalLength + (iface.equals("\\Device\\NPF_Loopback") ? 4 : 14)];
+        /* send to all*/
+        byte[] ipDestinationBytes = InetAddress.getByName("255.255.255.255").getAddress();
+        byte[] ipSourceBytes = InetAddress.getByName("127.0.0.1").getAddress();
+//        byte[] ipSourceBytes = InetAddress.getByName(ipSource).getAddress();
+        int addCount = 0;
+
+        /* set NPF_Loopback as iface to use. WORKS ONLY FOR WINDOWS*/
+        for (int i = 0, j = 7; i < 1; i++, j++) packet[i] = longToBytes(0x02)[j];
+
+        //Header Length = 20 bytes
+        for (int i = 4 + addCount, j = 7; i < 5 + addCount; i++, j++) packet[i] = longToBytes(69)[j];
+        //Differentiated Services Field
+        for (int i = 5 + addCount, j = 7; i < 6 + addCount; i++, j++) packet[i] = longToBytes(0x00)[j];
+        //Total Length
+        for (int i = 6 + addCount, j = 6; i < 8 + addCount; i++, j++) packet[i] = longToBytes(totalLength)[j];
+        //Identification - for fragmented packages
+        for (int i = 8 + addCount, j = 6; i < 10 + addCount; i++, j++) packet[i] = longToBytes(33500)[j];
+        //Flag, Fragment Offset - for fragmented packages
+        for (int i = 10 + addCount, j = 6; i < 12 + addCount; i++, j++) packet[i] = longToBytes(0x00)[j];
+        //Time to Live - max limit for moving through the network
+        for (int i = 12 + addCount, j = 7; i < 13 + addCount; i++, j++) packet[i] = longToBytes(128)[j];
+        //Protocol - UDP
+        for (int i = 13 + addCount, j = 7; i < 14 + addCount; i++, j++) packet[i] = longToBytes(17)[j];
+        //Header Checksum, can be 0x00 if it is not calculated
+        for (int i = 14 + addCount, j = 6; i < 16 + addCount; i++, j++) packet[i] = longToBytes(0x00)[j];
+
+        for (int i = 16 + addCount, j = 0; i < 20 + addCount; i++, j++) packet[i] = ipSourceBytes[j];
+        for (int i = 20 + addCount, j = 0; i < 24 + addCount; i++, j++) packet[i] = ipDestinationBytes[j];
+        //Source port
+        for (int i = 24 + addCount, j = 6; i < 26 + addCount; i++, j++) packet[i] = longToBytes(portToSend)[j];
+        //Destination port
+        for (int i = 26 + addCount, j = 6; i < 28 + addCount; i++, j++) packet[i] = longToBytes(portToSend)[j];
+        //Length
+        int length = totalLength - 20;
+        for (int i = 28 + addCount, j = 6; i < 30 + addCount; i++, j++) packet[i] = longToBytes(length)[j];
+        //Checksum, can be 0x00 if it is not calculated
+        for (int i = 30 + addCount, j = 6; i < 32 + addCount; i++, j++) packet[i] = longToBytes(0x0000)[j];
+        //Data
+        System.arraycopy(platformDescBytes, 0, packet, 32 + addCount, platformDescBytes.length);
+        return packet;
+    }
+
+
+    private byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+
+}
---
+++ src/main/java/org/example/PcapHelper.java
@@ -0,0 +1,102 @@
+package org.example;
+
+import com.sun.jna.NativeLibrary;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.pcap4j.core.*;
+
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class PcapHelper {
+
+    static {
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            NativeLibrary.addSearchPath("wpcap", "C:\\Windows\\System32\\Npcap");
+        }
+    }
+    private final PcapHandle pcapHandle;
+
+    /**
+     * @param ifaceName - interface name, which is used to capture packets
+     * @param T - time period to get packets from network card buffer in ms (use 50 - 100 ms)
+     */
+    public PcapHelper(String ifaceName, int T) {
+        pcapHandle = openIFaceToCapturePackets(ifaceName, T);
+    }
+
+    /**
+     * send packet into network
+     * @param packet - packet raw data
+     */
+    public void sendPacket(byte[] packet){
+        try {
+            pcapHandle.sendPacket(packet);
+        } catch (Exception e) {
+            log.error("Error during packet sending {}",e.toString());
+        }
+    }
+
+    /**
+     * @param port - port to use as filter to capture UDP packets
+     * @param pl - Packet listener, called when required UDP packet received. it must contain logic to handle new data item
+     * @param ses - executer to use to handle packets. NOTE : it occupies a thread
+     * @return - task of packets capturing
+     * @throws PcapNativeException  if pcap can not open found interface handle
+     */
+    public ScheduledFuture<?> startPacketsCapturing(int port,  PacketListener pl, ScheduledExecutorService ses) {
+        return startCapturingTask(port, pl, ses, pcapHandle);
+
+    }
+
+    private ScheduledFuture<?> startCapturingTask(int port, PacketListener pl, ScheduledExecutorService ses, PcapHandle pcapHandle) {
+        return ses.schedule(() -> {
+            try {
+                pcapHandle.setFilter("ip proto \\udp && dst port " + port, BpfProgram.BpfCompileMode.NONOPTIMIZE);
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("error during filter applying {}", e.toString());
+            }
+            try {
+                pcapHandle.loop(0, pl);
+            } catch (Exception e) {
+                e.printStackTrace();
+                log.error("error during grabbing packets{}", e.toString());
+            }
+        }, 0, TimeUnit.MILLISECONDS);
+    }
+
+    @SneakyThrows
+    private PcapHandle openIFaceToCapturePackets(String ifaceName, int T) {
+        List<PcapNetworkInterface> foundIfaces;
+        try {
+            foundIfaces = Pcaps.findAllDevs();
+        } catch (PcapNativeException e) {
+            log.error("lib pcap works incorrect. unable to find active ethernet interfaces");
+            throw new RuntimeException("can not call function find all Devs from pcap lib");
+        }
+
+        if (foundIfaces == null || foundIfaces.size() == 0) {
+            log.error("libPcap could not discover ethernet interfaces");
+            throw new RuntimeException("libPcap could not discover ethernet interfaces");
+        }
+
+        PcapNetworkInterface chosenIface = null;
+        for (PcapNetworkInterface ifs : foundIfaces) {
+            if (ifs.getName().equals(ifaceName)) {
+                chosenIface = ifs;
+                break;
+            }
+        }
+        if (chosenIface == null) {
+            log.error("Failed to find iface with name{}  ethernet available interface are : {}", ifaceName, foundIfaces.stream().map(el -> el.getName() + el.getAddresses()).collect(Collectors.toList()));
+            throw new RuntimeException("Can not find iface with given name "+ ifaceName);
+        }
+
+        return chosenIface.openLive(65536, PcapNetworkInterface.PromiscuousMode.PROMISCUOUS, T / 2);
+    }
+}
